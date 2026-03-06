4c457b8aead9712f1e90d4e825a2105211922eb1
Dharani-dotcom <bharathidharani52@gmail.com>
2026-03-06 20:23:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -253,6 +253,9 @@ def download_csv():
     )
 
 # ---------------- RUN ----------------
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render automatically sets PORT
+    app.run(host="0.0.0.0", port=port, debug=True)
 
